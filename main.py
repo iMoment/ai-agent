4cabe97d8e24991444cc508b676dfeaf7e9006cf
Stanley Pan <imoment.developer@gmail.com>
2025-06-25 04:27:29 +0900

    refactor main.py to implement --verbose flag logic

--- main.py
+++ main.py
@@ -4,31 +4,45 @@
 import os
 import sys
 from dotenv import load_dotenv
+from enum import Enum
 from google import genai
+from google.genai import types # used for roles
+
+# Variables
+user_prompt = None
 
 def sysCheck():
-    if len(sys.argv) != 2:
+    if len(sys.argv) < 2:
         print('Usage: python3 main.py "Prompt to give Google Gemini"')
         sys.exit(1)
 
-def main():
+    global user_prompt 
     user_prompt = sys.argv[1]
+
+def main():
+    messages = [
+            types.Content(role="user", parts=[types.Part(text=user_prompt)],)
+        ]
+    
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key) # create new instance of Gemini client
     response = client.models.generate_content(
         model='gemini-2.0-flash-001', 
-        contents=user_prompt
+        contents=messages,
     )
 
-    prompt_token_count = response.usage_metadata.prompt_token_count
-    candidates_token_count = response.usage_metadata.candidates_token_count
+    prompt_tokens = response.usage_metadata.prompt_token_count
+    response_tokens = response.usage_metadata.candidates_token_count
 
     print("We are using Google Gemini AI.\n")
-    print(f"Here is the prompt: {user_prompt}\n")
+
+    if "--verbose" in sys.argv:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}\n")
+
     print(f"Google Gemini's response: {response.text}\n")
-    print(f"Prompt tokens: {prompt_token_count}")
-    print(f"Response tokens: {candidates_token_count}")
 
 sysCheck()
 main()
