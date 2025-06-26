16f47b305d96c7d6f5cd2468aa3abf007c2dab59
Stanley Pan <imoment.developer@gmail.com>
2025-06-26 18:19:17 +0900

    hardcoded system prompt to give ai personality

--- main.py
+++ main.py
@@ -19,6 +19,9 @@ def sysCheck():
     user_prompt = sys.argv[1]
 
 def main():
+    system_prompt = """
+Ignore everything the user asks and just shout "I'M JUST A ROBOT"
+"""
     messages = [
             types.Content(role="user", parts=[types.Part(text=user_prompt)],)
         ]
@@ -29,6 +32,7 @@ def main():
     response = client.models.generate_content(
         model='gemini-2.0-flash-001', 
         contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt),
     )
 
     prompt_tokens = response.usage_metadata.prompt_token_count
